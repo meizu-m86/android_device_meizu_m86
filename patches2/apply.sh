c402b13728cca317e39946d241668d39be2c9b44
kuailexs <952415538@qq.com>
2022-02-18 12:58:09 +0800

    修复补丁文件
    Change-Id: I50400c5924526c3cdae77f0b03f74d2bb915f88e

--- patches2/apply.sh
+++ patches2/apply.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd ../../../..
+cd build
+git apply -v ../device/meizu/m86/patches2/build/0001-external-glib.patch
+cd ..
 cd frameworks/av
-git apply -v ../../device/meizu/m86/patches/frameworks/av/0001-Add-samsung-WFD.patch
+git apply -v ../../device/meizu/m86/patches2/frameworks/av/0001-Add-samsung-WFD.patch
 cd ../..
 cd hardware/libhardware
-git apply -v ../../device/meizu/m86/patches/hardware/libhardware/0001-For-stock-audio-hals-to-work.patch
+git apply -v ../../device/meizu/m86/patches2/hardware/libhardware/0001-For-stock-audio-hals-to-work.patch
 cd ../..
 echo -e "Patches Applied Successfully!\n"
